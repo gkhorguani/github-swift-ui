b0ea1fe1e56312140085f88078027986d3d6d2f9
Giorgi Khorguani <giorgikhorguani@Giorgis-MBP.attlocal.net>
2019-12-27 21:04:45 -0600

    First commit

---
+++ GithubClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubClient
+//
+//  Created by Giorgi Khorguani on 12/27/19.
+//  Copyright © 2019 Giorgi Khorguani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
